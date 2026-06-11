d37a766ac91c80e49dbcd5dddb3025cb23492c08
Akshat-Singh29 <akshatksingh29@gmail.com>
2026-06-11 20:02:36 +0530

    Add multithreaded parking lot management system
    Implemented a multithreaded parking lot management system with synchronized parking method.

--- Multithreading/Parking_lot_Management_System.java
+++ Multithreading/Parking_lot_Management_System.java
@@ -1,4 +1,62 @@
-/*
+// ParkingLot Class
+class ParkingLot {
+    private int availableSlots = 3;
+
+    // Synchronized method to ensure only one car parks at a time
+    public synchronized void parkCar(String carName) {
+
+        if (availableSlots > 0) {
+            System.out.println(carName + " Parked");
+
+            availableSlots--;
+
+            System.out.println("Available Slots: " + availableSlots);
+        } else {
+            System.out.println("No Parking Available for " + carName);
+        }
+    }
+}
+
+// CarThread Class
+class CarThread extends Thread {
+    private ParkingLot parkingLot;
+    private String carName;
+
+    public CarThread(ParkingLot parkingLot, String carName) {
+        this.parkingLot = parkingLot;
+        this.carName = carName;
+    }
+
+    @Override
+    public void run() {
+        parkingLot.parkCar(carName);
+    }
+}
+
+// Main Class
+public class ParkingSystem {
+    public static void main(String[] args) {
+
+        // Create shared ParkingLot object
+        ParkingLot parkingLot = new ParkingLot();
+
+        // Create 6 car threads
+        CarThread car1 = new CarThread(parkingLot, "Car 1");
+        CarThread car2 = new CarThread(parkingLot, "Car 2");
+        CarThread car3 = new CarThread(parkingLot, "Car 3");
+        CarThread car4 = new CarThread(parkingLot, "Car 4");
+        CarThread car5 = new CarThread(parkingLot, "Car 5");
+        CarThread car6 = new CarThread(parkingLot, "Car 6");
+
+        // Start all car threads
+        car1.start();
+        car2.start();
+        car3.start();
+        car4.start();
+        car5.start();
+        car6.start();
+    }
+}/*
 A shopping mall has:
 
 Available Parking Slots = 3
